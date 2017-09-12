0556420ee1481ccfad8eb974f0d60f187ac5eba4
Marlon O'Neil <hello@marlononeil.com>
2017-09-12 17:26:58 -0400

    completed restful routes for users, owners, reservations and restaurants models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :users, only: [:new, :create, :edit, :update]
+  resource :owners, only: [:new, :create, :edit, :update]
+  resources :reservations
+  resources :restaurants
+
+
 end
