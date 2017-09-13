43162d7258706a972f81c221ec4a07660c71f378
KaiserChan <chankaiserkk@gmail.com>
2017-09-13 18:40:48 -0400

    updated seatyourself ... updated seeds, updated migration, changed whole lots of methods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
+
+  SLOT_SIZE = 7200
+
   has_many :reservations
   belongs_to :owner
 
@@ -11,6 +14,28 @@ class Restaurant < ApplicationRecord
     end
   end
 
+  def time_slots
 
+    result = []
+
+    current_time = opening_time
+    while current_time <= closing_time
+      result << current_time.strftime("%I%p")
+      current_time += SLOT_SIZE
+    end
+
+    # num_of_slots = ( ((closing_time - opening_time)-1) / SLOT_SIZE )
+    puts "In time slots method"
+
+    # num_of_slots.to_i.times do
+    #   puts "slot = #{opening_time} += #{SLOT_SIZE}"
+    #   slot = opening_time += SLOT_SIZE
+    #   result << slot
+    # end
+
+
+
+    result
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,8 @@ User.create(first_name: "Kaiser", last_name: "Chan", email: "kaiser.chan@email.c
 
 Owner.create(name: "Marius Butuc", email: "marius@owners.com", password: "password")
 
-Owner.first.restaurants.create(name: "Marius' Delight", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", street_address: "165 Legion Rd N", city: "Etobicoke", province: "Ontario", postal_code: "M8Y 0B3", phone: "1112345678", max_capacity: 100, opening_time: Time.parse("06:00"), closing_time: Time.parse("20:00") )
+Owner.first.restaurants.create(name: "Marius' Delight", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", street_address: "165 Legion Rd N", city: "Etobicoke", province: "Ontario", postal_code: "M8Y 0B3", phone: "1112345678", max_capacity: 100, opening_time: Time.parse("6AM"), closing_time: Time.parse("8PM") )
+
+restaurant = Restaurant.last
+
+restaurant.reservations.create(reservation_date: Date.current + 1.day, reservation_time: Time.parse("5PM"), party_size: 10, user_id: User.first)
