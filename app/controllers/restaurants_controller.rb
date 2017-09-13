8e41747d14f7e5861b9f870f022c63b28c15441d
KaiserChan <chankaiserkk@gmail.com>
2017-09-13 12:14:26 -0400

    updated seatyourself - updated restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,29 @@
 class RestaurantsController < ApplicationController
 
+  SLOT_SIZE = 3600
+
+
   def index
-    @restaurants = Restaurant.search(params[:search])
+    # @restaurants = Restaurant.search(params[:search])
+
+    @restaurants = Restaurant.all
+    if params[:search]
+      @restaurants = Restaurant.search(params[:search])
+    else
+      @restaurants = Restaurant.all
+    end
   end
 
   def show
-    #code
+    @restaurant = Restaurant.find(params[:id])
+    @reservations = @restaurant.reservations
+    @total_slots = []
+    @num_of_slots = ( (@restaurant.closing_time - @restaurant.opening_time) / SLOT_SIZE )
+
+    @num_of_slots.to_i.times do
+      slot = @restaurant.opening_time += SLOT_SIZE
+      @total_slots << slot
+    end
   end
 
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,12 +2,15 @@ class Restaurant < ApplicationRecord
   has_many :reservations
   belongs_to :owner
 
+
   def self.search(search)
     if search
-      find(Restaurant.all, :conditions => ['name LIKE ?', "%#{search}%"])
+      find(Restaurant.all, :conditions => ["name LIKE ?", "%#{search}%"])
     else
       find(Restaurant.all)
     end
   end
+
+
 
 end
