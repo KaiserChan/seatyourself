d3c6b538dac56acafa41a66d28b4536dd19bbe21
Marlon O'Neil <hello@marlononeil.com>
2017-09-13 13:52:06 -0400

    created seeds for database, calculated the available dates from which to pick

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
 
   SLOT_SIZE = 3600
-
+  ALLOWED_DATES = 14
 
   def index
     # @restaurants = Restaurant.search(params[:search])
@@ -18,9 +18,22 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @reservations = @restaurant.reservations
     @total_slots = []
-    @num_of_slots = ( (@restaurant.closing_time - @restaurant.opening_time) / SLOT_SIZE )
+    @total_available_dates = []
 
-    @num_of_slots.to_i.times do
+
+    today = Date.current
+    count = 1
+    while count < ALLOWED_DATES
+      future_day = today += 1
+      @total_available_dates << future_day.strftime("%A %dth %B")
+      count += 1
+    end
+
+
+    num_of_slots = ( (@restaurant.closing_time - @restaurant.opening_time) / SLOT_SIZE )
+
+
+    num_of_slots.to_i.times do
       slot = @restaurant.opening_time += SLOT_SIZE
       @total_slots << slot
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Owner.delete_all
+Restaurant.delete_all
+Reservation.delete_all
+
+
+User.create(first_name: "Marlon", last_name: "O'Neil", email: "marlon.oneil@email.com", password: "password", phone: "55512345678", city: "Ottawa", province: "Ontario")
+
+User.create(first_name: "Kaiser", last_name: "Chan", email: "kaiser.chan@email.com", password: "password", phone: "5559876543", city: "Toronto", province: "Ontario")
+
+Owner.create(name: "Marius Butuc", email: "marius@owners.com", password: "password")
+
+Owner.first.restaurants.create(name: "Marius' Delight", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", street_address: "165 Legion Rd N", city: "Etobicoke", province: "Ontario", postal_code: "M8Y 0B3", phone: "1112345678", max_capacity: 100, opening_time: Time.parse("06:00"), closing_time: Time.parse("20:00") )
