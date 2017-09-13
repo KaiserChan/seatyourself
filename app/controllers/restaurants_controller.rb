76acf22614c3ef5ed2e031d9939240a0710b7649
KaiserChan <chankaiserkk@gmail.com>
2017-09-13 10:49:04 -0400

    updated seatyourself - added index method and created search form for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,13 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.search(params[:search])
+  end
+
+  def show
+    #code
+  end
+
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,13 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
   belongs_to :owner
+
+  def self.search(search)
+    if search
+      find(Restaurant.all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(Restaurant.all)
+    end
+  end
+
 end
