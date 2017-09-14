58409d4be26dff08d25dea3a4450c7f057f92378
KaiserChan <chankaiserkk@gmail.com>
2017-09-14 14:02:59 -0400

    updated seatyourself - created owner sign up and log in and respective views

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,35 @@
 class OwnersController < ApplicationController
+
+  def new
+    @owner = Owner.new
+  end
+
+  def create
+    @owner = Owner.new
+
+    @owner.name = params[:owner][:name]
+    @owner.email = params[:owner][:email]
+    @owner.password = params[:owner][:password]
+    @owner.password_confirmation = params[:owner][:password_confirmation]
+
+    if @owner.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  def edit
+    #code
+  end
+
+  def show
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    #code
+  end
+
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,19 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to users_url, notice: "Logged in!"
-    else
-      render "new"
+
+    end
+
+    owner = Owner.find_by(email: params[:email])
+    if owner && owner.authenticate(params[:password])
+      session[:owner_id] = owner.id
+      redirect_to owners_url, notice: "Logged in!"
     end
   end
 
   def destroy
-    session[:user_id] = nil
+    session[:user_id] = nil || session[:owner_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end
+  
 end
