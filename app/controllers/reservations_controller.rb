76bd33278fbcb83776e77929d6a346aaeb0a934f
Marlon O'Neil <hello@marlononeil.com>
2017-09-14 00:04:23 -0400

    Adjusted reservations/new.html.erb page style

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,11 +8,36 @@ class ReservationsController < ApplicationController
     # byebug
     @restaurant = Restaurant.find(params[:restaurant_id])
     @reservation = @restaurant.reservations.new
+
+    @reservation.reservation_date = params[:post][:reservation_date]
+    @reservation.party_size = params[:party_size]
+    @reservation.restaurant_id = params[:restaurant_id]
     #
     # @available_times = @reservation.available_times(params[:post][:reservation_date])
   end
 
   def create
+    @restaurant = Restaurant.find(params[:reservation][:restaurant_id])
+    @reservation = @restaurant.reservations.new
+
+    @reservation.reservation_date = params[:reservation][:reservation_date]
+    @reservation.party_size = params[:reservation][:party_size]
+    @reservation.restaurant_id = params[:reservation][:restaurant_id]
+    @reservation.reservation_time = params[:commit]
+
+    @reservation.save
+    redirect_to root_path
+
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:reservation][:restaurant_id])
+    @reservation = @restaurant.reservations.new
+
+    @reservation.reservation_date = params[:reservation][:reservation_date]
+    @reservation.party_size = params[:reservation][:party_size]
+    @reservation.restaurant_id = params[:reservation][:restaurant_id]
+    @reservation.reservation_time = params[:commit]
 
   end
 
