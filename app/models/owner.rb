d9019eab98ebfee8e42bdd651e11d9d1309d5fc0
KaiserChan <chankaiserkk@gmail.com>
2017-09-14 14:15:59 -0400

    updated seatyourself - added validations for user and owner creations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,10 @@
 class Owner < ApplicationRecord
   has_secure_password
 
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
+
+
+
   has_many :restaurants
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, :last_name, :email, :password, :phone, :city, :province, presence: true
+  validates :email, uniqueness: true
+  validates :phone, numericality: true
+
+
   has_many :reservations
 end
