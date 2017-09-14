a116c6ff492fb7fad2ffc5382d127fde6f2d75c7
Marlon O'Neil <hello@marlononeil.com>
2017-09-14 01:37:44 -0400

    Added restaurant to seeds file in order to test time slot creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,8 @@ Owner.create(name: "Marius Butuc", email: "marius@owners.com", password: "passwo
 
 Owner.first.restaurants.create(name: "Marius' Delight", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", street_address: "165 Legion Rd N", city: "Etobicoke", province: "Ontario", postal_code: "M8Y 0B3", phone: "1112345678", max_capacity: 100, opening_time: Time.zone.parse("8:00"), closing_time: Time.zone.parse("20:00") )
 
+Owner.first.restaurants.create(name: "Marius' Second Delight", summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", street_address: "165 Legion Rd N", city: "Ottawa", province: "Ontario", postal_code: "M8Y 0B3", phone: "9991239991", max_capacity: 100, opening_time: Time.zone.parse("9:00"), closing_time: Time.zone.parse("21:00") )
+
 restaurant = Restaurant.last
 
 restaurant.reservations.create(reservation_date: Date.current + 1.day, reservation_time: Time.zone.parse("5PM"), party_size: 10, user_id: User.first)
